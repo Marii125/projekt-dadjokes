dbf7a0122b80b6d71456bd56905607268a6d61e9
Martina Bujasová <martina.bujasova2@gmail.com>
2024-06-01 14:37:35 +0200

    Update: 16. pokracovani vtipu

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './style.css';
+import { Joke } from '../../components/Joke';
 
 export const HomePage = () => {
-  const [like, setLike] = useState(0);
-  const [down, setDown] = useState(0);
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    const fetchJokes = async () => {
+      const response = await fetch('http://localhost:4000/api/joke');
+      const data = await response.json();
+      setJokes(data.data);
+    };
+
+    fetchJokes();
+  }, []);
 
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
+      {jokes.map((joke) => {
+        return (
+          <Joke
+            key={joke.id}
+            userAvatar={joke.avatar}
+            userName={joke.name}
+            text={joke.text}
+            likes={joke.likes}
+            dislikes={joke.dislikes}
+          />
+        );
+      })}
+
+      {/* 
+     
+        
+       <div className="joke__body">
           <div className="joke__user">
             <img className="user-avatar" src="/pages/HomePage/img/user01.png" />
             <p className="user-name">Neroxx</p>
@@ -37,8 +62,8 @@ export const HomePage = () => {
           <span id="likes-down" className="likes-count likes-count--down">
             {down}
           </span>
-        </div>
-      </div>
+        </div> 
+      </div>*/}
     </div>
   );
 };
